676321f1ed56dba8455e5a7eb666862f6448a987
bluedr <skordos88@gmail.com>
2023-11-26 03:32:29 +0200

    'dfgdgasgdas'

--- app/Http/Controllers/settingController.php
+++ app/Http/Controllers/settingController.php
@@ -9,12 +9,19 @@ use Illuminate\Database\Eloquent\Collection;
 class settingController extends Controller
 {
     public function index(Request $request) {
-        $sal = sallary::all();
+        // $sal = sallary::all();
         $username = Auth::user()->name;
         $userid = Auth::user()->id;
-        $sal = sallary::find($userid);  
-        if(!empty($sal)) 
+        // $sal = sallary::find($userid);  
+        $sal = sallary::where('user_id',$userid)->first(); 
+        echo $sal; 
+        // if(!empty($sal)) {
+        if ($sal) { //todo na do pos tha kano to update ston pinaka
+            
+            // $sal->update(['sallary'=>$request->input('sallary')]);  
             return view('settings')->with('username',$username)->with('sallary',$sal->sallary); 
-        return view('settings')->with('username',$username);  
+        } else {
+            return view('settings')->with('username',$username);
+        }  
     }
 }
